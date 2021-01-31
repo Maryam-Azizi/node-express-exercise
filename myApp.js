f9ed76d8bcea2c13fa229b0e3fa2e30876f0014a
Maryam Azizi <azizi.m1390@gmail.com>
2021-01-31 04:08:17 +0000

    env variables

--- myApp.js
+++ myApp.js
@@ -1,21 +1,15 @@
 var express = require('express');
 var app = express();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.get("/json", (req, res) => {
+  var mymessage = process.env.GREETING + ' json';
+  if (process.env.MESSAGE_STYLE === "uppercase"){
+    mymessage = mymessage.toUpperCase();
+  }
+  res.json({
+    message: mymessage
+  });
+});
 
 
 
